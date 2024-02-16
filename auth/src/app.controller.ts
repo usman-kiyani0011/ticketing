4cb26bc54a464929b88710ca7f31d7716520ffe0
Usman Kiyani <usman.kiyani@ceative.co.uk>
2024-02-16 21:51:19 +0500

    added ingress controller

--- auth/src/app.controller.ts
+++ auth/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api/users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('currentuser')
   getHello(): string {
     return this.appService.getHello();
   }
